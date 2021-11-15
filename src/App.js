3016e0efc5a325604f5750bd0d4280eb9c249b80
Ben Hart <Benjmhart@gmail.com>
2021-11-15 09:48:48 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+
+const connect = (setState) => {
+  console.log('cardano object', window.cardano)
+  window.cardano.enable()
+    .then(api => {
+      console.log("enabled!", api)
+      // 'api' should have these methods, it appears nami glues these methods to
+      // the 'cardano' object after activation,deviating from the current
+      // version of the spec
+      return window.cardano.getUsedAddresses()
+    }).then(addrs => {
+      // addrs is an array-like object containing CBOR encoded addresses
+      // might decode using https://github.com/kriszyp/cbor-x 
+      console.log ("addresses returned", addrs)
+      setState({ address: addrs[0] })
+      console.log("done")
+    })
+    .catch(err => console.log("connect error", err))
+}
+
+const App = () => {
+  const [state, setState] = useState({ address: "" });
+  console.log("state", state)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => connect(setState)}>
+        Click to activate Nami Wallet
+      </button>
+      <div>Address: {state.address} </div>
     </div>
   );
 }
