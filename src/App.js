7042e79ba8b75a777284af4f32b361d5f9f8dc70
Mikhail Lazarev <mikhail@mlabs.city>
2021-11-30 23:00:28 +0300

    wip -deps added -sign added

--- src/App.js
+++ src/App.js
@@ -21,6 +21,15 @@ const connect = (setState) => {
     .catch(err => console.log("connect error", err))
 }
 
+//
+
+const sign = (tx) => {
+  console.log("sign txn")
+  window.cardano.signTx(tx)
+    .then(result => console.log("Success"))
+    .catch(err => console.log("error: ", err))
+}
+
 const App = () => {
   const [state, setState] = useState({ address: "" });
   console.log("state", state)
@@ -28,6 +37,9 @@ const App = () => {
     <div className="App">
       <button onClick={() => connect(setState)}>
         Click to activate Nami Wallet
+      </button>
+      <button onClick={() => sign("84a50081825820fd1e09ea0960dc55ae0cfbfc8ad93928d06d232db430a25b16c5dfeae5bff3fe000d81825820fd1e09ea0960dc55ae0cfbfc8ad93928d06d232db430a25b16c5dfeae5bff3fe00018282583900a3d0b159b730b4d28907681823a6ca155ce50f8614e00325daa85bd1da859f3b7756c74bd1f92e41928a5af7aaeddb07d89af26c757437aa1a3b1175c082581d605feb72668e5a4effd3b34088aa56e7d558f75afe4798b8364a7396731a006acfc0021a001e84800e80a0f5f6")}>
+        Sign
       </button>
       <div>Address: {state.address} </div>
     </div>
