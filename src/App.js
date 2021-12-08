91c8c5b39b4cb6e84c3265026b4c6ec74547d400
Mikhail Lazarev <mikhail@mlabs.city>
2021-12-08 16:11:18 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -6,8 +6,18 @@ import { Buffer } from 'buffer/'
 const hexToBytes = (hex) => Buffer.from(hex, "hex");
 const bytesToHex = (bytes) => Buffer.from(bytes).toString("hex");
 const utxoFromHex = (hex) => TransactionUnspentOutput.from_bytes(hexToBytes(hex));
-const getTxId  = (utxo) => bytesToHex(utxo.input().transaction_id().to_bytes());
+const getTxId = (utxo) => bytesToHex(utxo.input().transaction_id().to_bytes());
 const getTxIndex = (utxo) => utxo.input().index();
+
+const hexUtxoToOutRef = (hex) => {
+  const utxo = utxoFromHex(hex)
+  return {
+    txOutRefId: {
+      getTxId: getTxId(utxo)
+    },
+    txOutRefIdx: getTxIndex(utxo)
+  }
+}
 
 const connect = (setState) => {
   console.log('cardano object', window.cardano)
@@ -61,28 +71,28 @@ const callLock = (inst) => {
   window.cardano.getCollateral()
     .then(utxos => {
       console.log("Collateral: ", utxos)
-      const cUtxo = utxoFromHex(utxos[0])
-      const hardcodedAmt = 11000000 
+      const hardcodedAmt = 12000000 
 
-      const data = {
-        lovelaceAmount: hardcodedAmt,
-        collateralRef: {
-            txOutRefId: {
-                getTxId: getTxId(cUtxo)
-                },
-            txOutRefIdx: getTxIndex(cUtxo)
-        }
-      }
-      console.log("Endpoint req: ", data)
-      fetch(`http://localhost:9080/api/contract/instance/${inst}/endpoint/lock`,
-        {
-          method: "POST",
-          headers: { "Content-type": "application/json" },
-          body: JSON.stringify(data)
-        }
-      ).then(res => console.log("Endpoint call res: ", res))
-      .then(() => getAndSingSubmit(inst))
-      .catch(err => console.log(err))
+      window.cardano.getUtxos()
+        .then(walletUtxos => {
+          const data = {
+            lovelaceAmount: hardcodedAmt,
+            collateralRef: hexUtxoToOutRef(utxos[0]),
+            spendableUtxos: walletUtxos.map(utxo => hexUtxoToOutRef(utxo))
+          }
+          console.log("Endpoint req: ", data)
+          fetch(`http://localhost:9080/api/contract/instance/${inst}/endpoint/lock`,
+            {
+              method: "POST",
+              headers: { "Content-type": "application/json" },
+              body: JSON.stringify(data)
+            }
+          ).then(res => console.log("Endpoint call res: ", res))
+            .then(() => getAndSingSubmit(inst))
+            .catch(err => console.log(err))
+
+        })
+
     }
     )
     .then(res => console.log(res))
