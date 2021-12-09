e6429b20a183bcbfb8b89881395d93e4e57d6999
Mikhail Lazarev <mikhail@mlabs.city>
2021-12-09 16:29:13 +0300

    rebuilding witness

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useState } from 'react'
-import { Transaction, TransactionWitnessSet, Address, TransactionUnspentOutput } from '@emurgo/cardano-serialization-lib-asmjs'
+import { Transaction, TransactionWitnessSet, PlutusScripts, PlutusList,NativeScripts, Address, TransactionUnspentOutput, Vkeywitnesses, Redeemers } from '@emurgo/cardano-serialization-lib-asmjs'
 import { Buffer } from 'buffer/'
 
 const hexToBytes = (hex) => Buffer.from(hex, "hex");
@@ -36,6 +36,7 @@ const connect = (setState) => {
       console.log("done")
       return addrs[0]
     })
+    // -------------
     .then(addr => {
       const sendingAddress = Address.from_bytes(hexToBytes(addr), "hex").to_bech32()
       console.log(sendingAddress)
@@ -48,7 +49,7 @@ const connect = (setState) => {
             tag: 'LockSpend'
           }
         }
-      
+
       console.log("Activation JSON: ", data)
       return fetch("http://localhost:9080/api/contract/activate",
         {
@@ -63,6 +64,7 @@ const connect = (setState) => {
       console.log("Data: ", data)
       setState({ contractInstance: data.unContractInstanceId })
     })
+    // ---------
     .catch(err => console.log("Error: ", err))
 }
 
@@ -71,7 +73,7 @@ const callLock = (inst) => {
   window.cardano.getCollateral()
     .then(utxos => {
       console.log("Collateral: ", utxos)
-      const hardcodedAmt = 12000000 
+      const hardcodedAmt = 12000000
 
       window.cardano.getUtxos()
         .then(walletUtxos => {
@@ -103,17 +105,17 @@ const getAndSingSubmit = (contractInstance) => {
   console.log("Contract instance: ", contractInstance)
   setTimeout(() => {
     fetch(`http://localhost:9080/api/contract/instance/${contractInstance}/status`)
-    .then(response => response.json())
-    .then(contractState => {
-      console.log("Contract state: ", contractState)
-      const cbor = contractState.cicYieldedExportTxs[0].transaction
-      console.log("CBOR: ", cbor)
-      return cbor
-    })
-    .then(cbor => signSubmit(cbor))
-    .catch(err => console.log(err))
+      .then(response => response.json())
+      .then(contractState => {
+        console.log("Contract state: ", contractState)
+        const cbor = contractState.cicYieldedExportTxs[0].transaction
+        console.log("CBOR: ", cbor)
+        return cbor
+      })
+      .then(cbor => signSubmit(cbor))
+      .catch(err => console.log(err))
   },
-  3000
+    3000
   )
 }
 
@@ -146,13 +148,71 @@ const sign = (tx) => {
 
 const dhc = (txCbor) => {
   console.log("Before: ", txCbor)
-  const txn = Transaction.from_bytes(Buffer.from(txCbor, 'hex'))
-  console.log("After: ", Buffer.from(txn.to_bytes()).toString('hex'))
-  // console.log("TXN 1: ", txn.body().outputs().get(0))
-  // console.log("TXN 2: ", txn.body().outputs().get(1))
-  // const witnessSet = TransactionWitnessSet.from_bytes(Buffer.from(wits, 'hex'))
-  // const signedTx = Transaction.new(txn.body(), witnessSet)
-  // console.log("Signed: ", Buffer.from(signedTx.to_bytes()).toString('hex'))
+  const backendTx = Transaction.from_bytes(Buffer.from(txCbor, 'hex'))
+  const backendTxWitnessSet = backendTx.witness_set()
+  console.log("Backend WS keys: ", backendTxWitnessSet.vkeys())
+  console.log("Backend WS dats: ", backendTxWitnessSet.plutus_data())
+
+  window.cardano.signTx(txCbor)
+    .then(namiWitnessSetHex => {
+      const namiWitnessSet = TransactionWitnessSet.from_bytes(Buffer.from(namiWitnessSetHex, 'hex'))
+      console.log("WS signed: ", namiWitnessSet)
+
+      console.log("native before: ", namiWitnessSet.native_scripts())
+      if (backendTxWitnessSet.native_scripts()) {
+        console.log("Setting nat-scripts: ", backendTxWitnessSet.native_scripts())
+        namiWitnessSet.set_native_scripts(
+          NativeScripts.from_bytes(backendTxWitnessSet.native_scripts().to_bytes())
+        );
+      }
+      console.log("native after: ", namiWitnessSet.native_scripts())
+
+      console.log("pscripts before: ", namiWitnessSet.plutus_scripts())
+      if (backendTxWitnessSet.plutus_scripts()) {
+        console.log("Setting scripts: ", backendTxWitnessSet.plutus_scripts())
+        namiWitnessSet.set_plutus_scripts(
+          PlutusScripts.from_bytes(backendTxWitnessSet.plutus_scripts().to_bytes())
+        );
+      }
+      console.log("pscripts after: ", namiWitnessSet.plutus_scripts())
+
+      console.log("datums before: ", namiWitnessSet.plutus_data())
+      if (backendTxWitnessSet.plutus_data()) {
+        console.log("Setting data: ", backendTxWitnessSet.plutus_data())
+        namiWitnessSet.set_plutus_data(
+          PlutusList.from_bytes(backendTxWitnessSet.plutus_data().to_bytes())
+        );
+      }
+      console.log("datums after: ", namiWitnessSet.plutus_data())
+
+
+      console.log("redeemers before: ", namiWitnessSet.redeemers())
+      if (backendTxWitnessSet.redeemers()) {
+        console.log("Setting redeemers: ", backendTxWitnessSet.redeemers())
+        namiWitnessSet.set_redeemers(
+          Redeemers.from_bytes(backendTxWitnessSet.redeemers().to_bytes())
+        );
+      }
+      console.log("redeemers after: ", namiWitnessSet.redeemers())
+
+      console.log("Vkeys: ", namiWitnessSet.vkeys())
+
+      const resTx = Transaction.new(
+        backendTx.body(),
+        namiWitnessSet,
+        // backendTx.auxiliary_data()
+      )
+
+      const resTxHex = Buffer.from(resTx.to_bytes()).toString('hex')
+      console.log("Res hex: ", resTxHex)
+
+      // ------ SUBMIT
+      window.cardano.submitTx(resTxHex)
+      .then(res => console.log("Submitted!"))
+      .catch(err => console.log("Submission error: ", err))
+      // ------ SUBMIT
+    })
+    .catch(err => console.log("error: ", err))
 }
 
 
@@ -179,7 +239,7 @@ const App = () => {
       <button onClick={() => callLock(state.contractInstance)}>
         Lock
       </button>
-      <button onClick={() => dhc("84a50081825820522b457e0d077d0f99c664426f4483daf3fc93ddea927bbac685b772921abda0000d81825820513aefa8cce5435985cef0795a96cbbd3937fce77ad1ed715ce6df77a15fe27f000182825839005feb72668e5a4effd3b34088aa56e7d558f75afe4798b8364a739673700e4b6993cbc5e612f8770aa531243b3d888a81c0a46de5fa5f6a701a362babc383581d70719ed02924d3a1656481f02af2adbdc229d14778b718e928695638d41a00b71b005820ff6191af0d39df3e972d56ecd3610316ff0ce4b1ab65a336a9adfea25c8aa3bc021a001e84800e80a10481d8799f46546573742031fff5f6")}>
+      <button onClick={() => dhc("84a50081825820eb87c8b20ccf3767e5b81cf30061f645871ee3c103c74d2f7f66d4364b7e5263000d81825820513aefa8cce5435985cef0795a96cbbd3937fce77ad1ed715ce6df77a15fe27f000182825839005feb72668e5a4effd3b34088aa56e7d558f75afe4798b8364a739673700e4b6993cbc5e612f8770aa531243b3d888a81c0a46de5fa5f6a701a35560c4383581d70719ed02924d3a1656481f02af2adbdc229d14778b718e928695638d41a00b71b005820ff6191af0d39df3e972d56ecd3610316ff0ce4b1ab65a336a9adfea25c8aa3bc021a001e84800e80a10481d8799f46546573742031fff5f6")}>
         Debug hardcoded CBOR
       </button>
       <button onClick={() => debg(setState)}>
